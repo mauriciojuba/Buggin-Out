07623285bdc32b5cf8c6f343aff21317afb22eda
Wauei <waueigt@gmail.com>
2017-09-16 22:00:43 -0300

    SFX Level 1-A

--- Assets/Scripts/Cutscenes/StartCutscene1A.cs
+++ Assets/Scripts/Cutscenes/StartCutscene1A.cs
@@ -11,7 +11,7 @@ public class StartCutscene1A : MonoBehaviour {
 		if(hit.CompareTag("Player1_3D")){
 			zoomOut = true;
 			Roomba.enabled = true;
-            SoundManager.PlaySFX("Roomba");
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Roomba", transform.position);
 		}
 	}
 	void Update(){
