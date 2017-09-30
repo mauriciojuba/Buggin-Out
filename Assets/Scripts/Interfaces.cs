5f25129372e0398bf4fbbe2e67305626642457e4
Mauricio Juba <mauriciojuba01@gmail.com>
2017-09-30 15:52:23 -0300

    IA Interfaces, Classe Genérica

---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IGroundEnemy
+{
+	void CheckAllTargets();
+	void Idle(Transform[] players);
+	void Patrol(float speed, Transform[] waypoints);
+	void Chase(float speed, Transform target);
+	void Flee(float speed, Transform nextPos);
+	void Attack(float hitPoints, GameObject player);
+}
+public interface IKillable
+{
+	void TakeDamage(float hitPoints);
+	void Die();
+}
+public interface IGoToScreen
+{
+	void UpToScreen();
+	void DownToGround();
+}
+public interface IScreenEntity{
+	void WalkOnScreen();
+	void AttackOnScreen();
+}
+public interface IAnimated<T>
+{
+	void Animate(T anim, string variable, float value);
+}
