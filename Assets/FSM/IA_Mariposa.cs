feef859bf7faf62c9d39d370a93092fdc08acda3
Wauei <waueigt@gmail.com>
2017-11-16 00:57:59 -0200

    Ajustes Mariposa

--- Assets/FSM/IA_Mariposa.cs
+++ Assets/FSM/IA_Mariposa.cs
@@ -6,14 +6,55 @@ public class IA_Mariposa : EnemyIA
 {
 
     public GameObject HitBox;
+    public float Time_atk_Area = 5F;
 
     public override void Attack()
     {
-        _anim.SetTrigger("ATK1");
+        _anim.SetTrigger("ATK2");
+
+        if (hitted) ActualState = State.TakeDamage;
 
         ActualState = State.Chase;
 
-        if (hitted) ActualState = State.TakeDamage;
+        
+
+    }
+
+    public override void Chase()
+    {
+
+        Time_atk_Area -= Time.deltaTime;
+        if(Time_atk_Area < 0)
+        {
+            ActualState = State.OnScreenAttack;
+        }
+
+        if (_anim != null)
+        {
+            _anim.SetBool("IsParolling", false);
+            _anim.SetBool("FightingWalk", true);
+        }
+        Vector3 dir = Target.transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Target.transform.position - transform.position), Time.deltaTime * rotationSpeed);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        if (targetDistance > EnemyDist && Vector3.Distance(Target.transform.position, gameObject.transform.position) < SafeDist)
+        {
+            RB.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+        }
+        attackTimer += Time.deltaTime;
+        if (targetDistance <= EnemyDist && attackTimer >= attackDelay)
+        {
+            ActualState = State.Attack;
+            attackTimer = 0;
+        }
+        if (targetDistance > SafeDist + 1)
+        {
+            if (_anim != null)
+                _anim.SetBool("FightingWalk", false);
+            ActualState = State.Patrol;
+        }
+        if (hitted)
+            ActualState = State.TakeDamage;
     }
 
     public override void OnScreenIdle()
@@ -23,13 +64,27 @@ public class IA_Mariposa : EnemyIA
    
     public override void OnScreenAttack()
     {
-       
+        _anim.SetTrigger("ATK1");
+
+        Time_atk_Area = 5F;
+
+        if (hitted) ActualState = State.TakeDamage;
+
+        ActualState = State.Chase;
     }
 
     public override void DownToGround()
     {
        
     }
+
+    public override void Die()
+    {
+        RB.isKinematic = true;
+        _anim.SetTrigger("Death");
+
+    }
+    
 
     public void OnTriggerExit(Collider hit){
     	if (hit.CompareTag ("playerHitCollider")) {
