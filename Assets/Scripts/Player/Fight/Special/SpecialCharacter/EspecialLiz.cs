5ebfdc054711722bddcde97a10e585ff7f97212c
EduardoBoaventura <eduardo.boaveentura@yahoo.com.br>
2017-11-15 22:18:57 -0200

    ajuste especial liz

--- Assets/Scripts/Player/Fight/Special/SpecialCharacter/EspecialLiz.cs
+++ Assets/Scripts/Player/Fight/Special/SpecialCharacter/EspecialLiz.cs
@@ -4,7 +4,34 @@ using UnityEngine;
 
 public class EspecialLiz : MonoBehaviour {
 
-    void OnTriggerEnter(Collider hit){
+    public float Damage = 25;
 
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    void OnTriggerEnter(Collider hit){
+        if (hit.CompareTag("Enemy"))
+        {
+            if (hit.GetComponent<IA_Mosquito>() != null)
+            {
+                hit.GetComponent<IA_Mosquito>().playerStr = Damage;
+                hit.GetComponent<IA_Mosquito>().hitted = true;
+                hit.GetComponent<IA_Mosquito>().TakeDamage();
+            }
+
+            if (hit.GetComponent<IA_Aranha>() != null)
+            {
+                hit.GetComponent<IA_Aranha>().Life -= Damage;
+                hit.GetComponent<IA_Aranha>().TakeDamage();
+            }
+
+            if (hit.GetComponent<IA_Mariposa>() != null)
+            {
+                hit.GetComponent<IA_Mariposa>().Life -= Damage;
+                hit.GetComponent<IA_Mariposa>().TakeDamage();
+            }
+        }
 	}
 }
