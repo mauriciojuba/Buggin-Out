f6427b7b7e587bd713ea18f8558a684086d66a97
Wauei <waueigt@gmail.com>
2017-11-28 20:38:12 -0200

    Ajustes Tutorial e Sons

---
+++ Assets/FSM/Enable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enable : MonoBehaviour
+{
+
+    public GameObject On;
+    public bool activated;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Horn"  || other.gameObject.name == "Liz")
+        {
+            On.gameObject.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
