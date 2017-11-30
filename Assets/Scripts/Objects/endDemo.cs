05ed1f4df5fec65d17ffbebafaf5b0d9d320db9d
EduardoBoaventura <eduardo.boaveentura@yahoo.com.br>
2017-11-30 01:08:19 -0200

    boss scene

--- Assets/Scripts/Objects/endDemo.cs
+++ Assets/Scripts/Objects/endDemo.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class endDemo : MonoBehaviour {
 
+    public string Level;
+
 	void OnTriggerEnter(Collider hit){
 		if(hit.CompareTag("Player1_3D") || hit.CompareTag("Player2_3D")){
             if (GameObject.FindWithTag("Loading") != null)
-                GameObject.FindWithTag("Loading").GetComponent<Loading>().StartCoroutine(GameObject.FindWithTag("Loading").GetComponent<Loading>().LoadAsync("Level 1B"));
+                GameObject.FindWithTag("Loading").GetComponent<Loading>().StartCoroutine(GameObject.FindWithTag("Loading").GetComponent<Loading>().LoadAsync(Level));
             else
-                UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Level 1B");
+                UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(Level);
         }
 	}
 }
--- Assets/Scripts/Player/Movement/JumpGravity.cs
+++ Assets/Scripts/Player/Movement/JumpGravity.cs
@@ -27,7 +27,7 @@ public class JumpGravity : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if (Moviment != null) {
 			if (!Moviment.natela) {
 				/*
