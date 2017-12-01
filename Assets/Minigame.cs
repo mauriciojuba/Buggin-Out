be4ef1d3b20c2769f3649e37c67564b4ea4b4356
EduardoBoaventura <eduardo.boaveentura@yahoo.com.br>
2017-12-01 18:08:34 -0200

    Minigame Juan
    Precisa arrumar os limites da fase

---
+++ Assets/Minigame.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame : MonoBehaviour {
+
+    [SerializeField] Collider _Col;
+    [SerializeField] float Timer;
+    [SerializeField] List<GameObject> AreaEnemies;
+    [SerializeField] bool Started;
+    [SerializeField] GameObject[] Prision;
+	void Start () {
+        for (int i = 0; i < AreaEnemies.Count; i++)
+        {
+            AreaEnemies[i].SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Started)
+        {
+            if (AreaEnemies.Count > 0)
+            {
+                if(Timer > 0)
+                Timer -= Time.deltaTime;
+                else
+                {
+                    Timer = 0;
+                }
+            }
+            for (int i = 0; i < AreaEnemies.Count; i++)
+            {
+                if (AreaEnemies[i].GetComponent<Waves>() != null)
+                {
+                    if (AreaEnemies[i].GetComponent<Waves>().AtualEnemy.Count == 0 && AreaEnemies[i].GetComponent<Waves>().QuantInimigo == 0)
+                    {
+                        AreaEnemies.RemoveAt(i);
+                    }
+                }
+            }
+
+            if(AreaEnemies.Count == 0 && Timer > 0)
+            {
+                for (int a = 0; a < Prision.Length; a++)
+                {
+                    Destroy(Prision[a]);
+                }
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player1_3D") || other.CompareTag("Player2_3D"))
+        {
+            for(int i = 0; i < AreaEnemies.Count; i++)
+            {
+                AreaEnemies[i].SetActive(true);
+            }
+            Started = true;
+            _Col.enabled = false;
+        }
+    }
+}
